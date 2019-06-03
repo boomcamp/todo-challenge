18b7dc99c8e4d53bc1135aba9c1fdf2f1c818c10
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-03 16:53:34 +0800

    refactored some code, removed bug where user can add multiple text boxes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     $( ".btn" ).click(function() {
-            $(".list").append('<textarea class="txtarea" rows="1" cols="30"></textarea>');
-            $('.txtarea').keypress(function(event){
-            var keycode = (event.keyCode ? event.keyCode : event.which);
+        $(".btn").attr("disabled", true);
+        $(".list").append('<textarea class="txtarea" rows="1" cols="30"></textarea>');
+        $('.txtarea').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
             if(keycode == '13'){
                 let message = $(this).val();
                 $(".list").append(`<div class="list-item"> ${message}</div>`);
                 $(".txtarea").remove();
+                $(".btn").attr("disabled", false);
             }
         });     
     });     
@@ -16,7 +18,6 @@ $( document).on( "click", ".list-item", function( event ) {
     event.preventDefault();
     event.stopPropagation();
     let message  =  $(this).text();
-    console.log('look' + message);
     $(".list-2").append(`<div class="list-item-2"> ${message}</div>`);
     $(this).fadeOut("normal", function() {
         $(this).remove();
@@ -27,7 +28,6 @@ $( document).on( "click", ".list-item-2", function( event ) {
     event.preventDefault();
     event.stopPropagation();
     let message  =  $(this).text();
-    console.log('look' + message);
     $(".list-3").append(`<div class="list-item-3"> ${message}</div>`);
     $(this).fadeOut("normal", function() {
         $(this).remove();
@@ -37,8 +37,6 @@ $( document).on( "click", ".list-item-2", function( event ) {
 $( document).on( "click", ".list-item-3", function( event ) {
     event.preventDefault();
     event.stopPropagation();
-    let message  =  $(this).text();
-    console.log('look' + message);
     $(this).fadeOut("normal", function() {
         $(this).remove();
     });
