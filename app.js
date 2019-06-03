c61e01f58f4c88afe12d34e6267bd3869cb16d7f
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-06-03 17:11:41 +0800

    adding task done

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    //hidden by default{
+    $('.NewTask-form').hide();
+
+    //}
+
+    ! function() {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var d = new Date();
+        var month = d.getMonth() + 1;
+        var year = d.getFullYear();
+        var dd = d.getDate();
+
+
+        $('.day').html(dd);
+        $('.year').html(year);
+        $('.month').html(monthNames[d.getMonth()]);
+        $('.week').html(dayNames[d.getDay()]);
+    }();
+
+    $('.add-todo').click(function() {
+        $('.NewTask-form').toggle();
+    })
+
+    $('#add2do').click(function() {
+        $('.todo-list').append(
+            '<div class="todo">' +
+            $('#todo-input').val() +
+            '</div>'
+        )
+        event.preventDefault();
+    })
+
+})
