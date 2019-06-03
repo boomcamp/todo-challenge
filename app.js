5e6aa855c81457571bcf4f4c00395acf7443d64e
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-03 16:19:39 +0800

    finished basic Todo, In Progress, and Done functionalities

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $( ".btn" ).click(function() {
-        alert( "Handler for .click() called." ); 
             $(".list").append('<textarea class="txtarea" rows="1" cols="30"></textarea>');
             $('.txtarea').keypress(function(event){
             var keycode = (event.keyCode ? event.keyCode : event.which);
@@ -19,6 +18,27 @@ $( document).on( "click", ".list-item", function( event ) {
     let message  =  $(this).text();
     console.log('look' + message);
     $(".list-2").append(`<div class="list-item-2"> ${message}</div>`);
+    $(this).fadeOut("normal", function() {
+        $(this).remove();
+    });
+});
+
+$( document).on( "click", ".list-item-2", function( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+    let message  =  $(this).text();
+    console.log('look' + message);
+    $(".list-3").append(`<div class="list-item-3"> ${message}</div>`);
+    $(this).fadeOut("normal", function() {
+        $(this).remove();
+    });
+});
+
+$( document).on( "click", ".list-item-3", function( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+    let message  =  $(this).text();
+    console.log('look' + message);
     $(this).fadeOut("normal", function() {
         $(this).remove();
     });
