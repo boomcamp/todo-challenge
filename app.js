1512297b96945458eded8ab9f3c93d61c9a4c39f
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-03 16:13:07 +0800

    added functionality: from todo to in progress

--- app.js
+++ app.js
@@ -6,25 +6,19 @@ $(document).ready(function(){
             var keycode = (event.keyCode ? event.keyCode : event.which);
             if(keycode == '13'){
                 let message = $(this).val();
-                $(".list").append(`<div class="list-item a href="#"> ${message}</div>`);
+                $(".list").append(`<div class="list-item"> ${message}</div>`);
                 $(".txtarea").remove();
             }
         });     
-    });
-
-    // $(".list-item").click(function(){
-    //     $(this).fadeOut("normal", function() {
-    //         $(this).remove();
-    //     });
-    //     });
-
-     
+    });     
 });
 
 $( document).on( "click", ".list-item", function( event ) {
     event.preventDefault();
-    let message = $(this).val();
-    $(".list-2").append(`<div class="list-item-2 a href="#"> ${message}</div>`);
+    event.stopPropagation();
+    let message  =  $(this).text();
+    console.log('look' + message);
+    $(".list-2").append(`<div class="list-item-2"> ${message}</div>`);
     $(this).fadeOut("normal", function() {
         $(this).remove();
     });
