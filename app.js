326dd2a5f8aeab7b9aefe914dd7c51e4b3d22811
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-03 15:46:15 +0800

    fixed minor bug that newly created element is not removed

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-$( ".btn" ).click(function() {
-    alert( "Handler for .click() called." ); 
-     $(".list").append('<textarea class="txtarea" rows="1" cols="30"></textarea>');
-     $('.txtarea').keypress(function(event){
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if(keycode == '13'){
-            let message = $(this).val();
-            $(".list").append(`<div class="list-item"> ${message}</div>`);
-            $(".txtarea").remove();
-        }
+$(document).ready(function(){
+    $( ".btn" ).click(function() {
+        alert( "Handler for .click() called." ); 
+            $(".list").append('<textarea class="txtarea" rows="1" cols="30"></textarea>');
+            $('.txtarea').keypress(function(event){
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if(keycode == '13'){
+                let message = $(this).val();
+                $(".list").append(`<div class="list-item a href="#"> ${message}</div>`);
+                $(".txtarea").remove();
+            }
+        });     
     });
-    
-     
-  });
 
-  
- 
+    // $(".list-item").click(function(){
+    //     $(this).fadeOut("normal", function() {
+    //         $(this).remove();
+    //     });
+    //     });
+
+     
+});
+
+$( document).on( "click", ".list-item", function( event ) {
+    event.preventDefault();
+    let message = $(this).val();
+    $(".list-2").append(`<div class="list-item-2 a href="#"> ${message}</div>`);
+    $(this).fadeOut("normal", function() {
+        $(this).remove();
+    });
+});
