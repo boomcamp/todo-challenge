7bbbc20f30d88dbc25e35da4139b7204248c5fc6
Emelyn Soria <50132975+emelynsoria@users.noreply.github.com>
2019-06-04 23:35:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var delBtn = '<img id="del" src="img/baseline-delete-24px.svg"/>';
 var editBtn = '<img id="edit" src="img/baseline-create-24px.svg"/>';
 var inProgBtn = '<img id="inProg" src="img/baseline-forward-24px.svg"/>';
 var toDoBtn = '<img id="backToDo" src="img/baseline-backward-24px.png"/>';
+var toProgBtn = '<img id="backToProg" src="img/baseline-backward-24px.png"/>';
 var doneBtn = '<img id="done" src="img/baseline-forward-24px.svg"/>';
 
     $(document).ready(function() {
@@ -14,28 +15,7 @@ var doneBtn = '<img id="done" src="img/baseline-forward-24px.svg"/>';
                 $('#del').click(function() {
                     $('#toDoLi').remove();
                 });  
-                //delete all
-                // $('#del').click(function() {
-                //     $('#toDoUl').empty();
-                // }); 
-                //  OR
-                // $('#del').click(function() {
-                //     $('#toDoUl li').remove();
-                // });
 
-                //transfer from 'To Do' to 'In Progress'
-                $('#inProg').click(function() {
-                    var toBeProg =  $('#del').replaceWith(toDoBtn);
-                    var toBeProg =  $('#inProg').replaceWith(doneBtn);
-                    var toBeProg =  $('#toDoLi').remove();
-                    toBeProg.prependTo('#inProgUl');
-
-                    // var toBeProg = $('#toDoLi:first').remove();
-                    // var newProg = $('<li id="inProgLi">' + toBeProg + doneBtn + toDoBtn + editBtn + '</li>');
-                    // $('#inProgUl').prepend(newProg);
-                });        
-
-                
                 $('#toDoUl').prepend(newLi);
                 //clear inputs
                 $('.addText').val('');
@@ -45,36 +25,55 @@ var doneBtn = '<img id="done" src="img/baseline-forward-24px.svg"/>';
                 $('.addBtn').prop('disabled', true);
                 alert('You must add a task first!');
             }
-
-            
-
         });
 
-        // function gobackTodo(target){
-        //     document.getElementById('toDoUl').append(target);
-        //     var lastChild = document.getElementById('toDoUl').lastChild;
-        //     lastChild.children[0].remove();
-        //     lastChild.children[0].insertAdjacentHTML("afterend", inProgBtn);
-        //     lastChild.children[0].remove();
-        // }
+        $(document).click(function() {
+            //from 'To Do' to 'In Progress'
+            $('#inProg').click(function() {
+                var toBeProg =  $('#del').replaceWith(toDoBtn);
+                var toBeProg =  $('#inProg').replaceWith(doneBtn);
+                var toBeProg =  $('#toDoLi').remove();
+                toBeProg.prependTo('#inProgUl');              
+            });
 
-        // $('.inProgUl').focus(function() {
-        //  $('#backToDo').click(function() {
-            //back to 'To Do'
-            // $('#backToDo').click(function() {
-            //     var toToDo =  $('#done').replaceWith(inProgBtn);
-            //     var toToDo =  $('#backToDo').replaceWith(delBtn);
-            //     var toToDo =  $('#backToDo').remove();
-            //     toToDo.prependTo('#toDoUl');
-            // });
+            //from 'In Progress' back to 'To Do'
+            $('#backToDo').click(function() {
+                var toToDo =  $('#done').replaceWith(inProgBtn);
+                var toToDo =  $('#backToDo').replaceWith(delBtn);
+                var toToDo =  $('#inProgUl li:first').remove();
+                toToDo.prependTo('#toDoUl');
+            });
 
-            // //to 'Done'
-            // $('#done').click(function() {
-            //     var toDone =  $('#done').replaceWith(delBtn);
-            //     var toDone =  $('#done').remove();
-            //     toDone.prependTo('#DoneUl');
-            // });
+            //from 'In Progress' to 'Done'
+            $('#done').click(function() {
+                var toDone =  $('#done').replaceWith(delBtn);
+                var toDone =  $('#backToDo').replaceWith(toProgBtn);
+                var toDone =  $('#inProgUl li:first').remove();
+                toDone.prependTo('#doneUl');
+            });
+
+            //from 'Done' back to 'In Progress'
+            $('#backToProg').click(function() {
+                var toProg =  $('#backToProg').replaceWith(toDoBtn);
+                var toProg =  $('#del').replaceWith(doneBtn);
+                var toProg =  $('#doneUl li:first').remove();
+                toProg.prependTo('#inProgUl');
+            });
+
+            //delete a task
+            $('#del').click(function() {
+                $('#doneUl li:first').remove();
+            });
+        });
+
+        // $('#edit').click(function() {
+        //     var str = $('#toDoLi').val();
+        //     $('#toDoLi').text(str);
         // });
-        
+
+        // $('#toDoUl:first #edit').click(function() {
+        //     var str = $('#toDoLi').val();
+        //     $('#toDoLi').text(str);
+        // });
         
     });
