2462c52ddc1574cac05337eb04b793735e389887
armanbayona <arman.bayona@boom.camp>
2019-06-04 12:55:42 +0800

    finished but no challenge

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-    //modal opens
+    
+    //opens the modal, prepared for adding
     $("#modalOpen").click(function(){
         console.log('modal');
         $(".modal > h2").text('Add Todo');
@@ -8,8 +9,8 @@ $(document).ready(function(){
         $('#editTodo').hide();
         $("#modal").slideDown("fast");
         $("#modalContainer").slideDown("fast");
-
     });
+
     var todoCount = 0;
     var todosArr = [];
     $(".newTodo").hide();
@@ -19,29 +20,44 @@ $(document).ready(function(){
         //get value of textarea
         var myTodo =  $("#text").val();
 
-        //clone the template
-        var template = $(".newTodo").clone();
-        $(".newTodo:first").show();
+        if(myTodo.length > 0){
+            //clone the template
+            var template = $(".newTodo").clone();
+            $(".newTodo:first").show();
 
-        //add todo
-        todoCount++;
-        var newClass = todoCount;
-        $(".newTodo:first").attr('id', newClass);
-        $("#"+newClass).removeClass('newTodo');
-        $("#"+newClass+">label").text(myTodo);
+            //add todo
+            todoCount++;
+            var newClass = todoCount;
+            $(".newTodo:first").attr('id', newClass);
+            $("#"+newClass).removeClass('newTodo');
+            $("#"+newClass+">label").text(myTodo);
+            $("#text").val('');
+            $("#modal").slideUp("fast");
+            $("#modalContainer").slideUp("fast");
 
-        $("#"+newClass+" .edit").attr('id', newClass);
-        $("#"+newClass+" .progress").attr('id', newClass);
-        $("#todos").prepend(template);
-        //console.log(todo)
-        //close modal, delete val of textarea
-        $("#text").val('');
-        $("#modal").slideUp("fast");
-        $("#modalContainer").slideUp("fast");
+            $("#"+newClass+" .edit").attr('id', newClass);
+            $("#"+newClass+" .stat_todo").attr('id', newClass); 
+            $("#"+newClass+" .stat_progress").attr('id', newClass); 
+            $("#"+newClass+" .stat_done").attr('id', newClass); 
+
+            $("#"+newClass+" .stat_todo").hide()
+            $("#"+newClass+" .stat_done").hide()
+
+            $("#"+newClass+" .delete").attr('id', newClass);
+            $("#todos").prepend(template).
+            $("#todos").slideDown('slow');
+            //console.log(todo)
+            //close modal, delete val of textarea
+           
+        }
+        else{
+            $("#modal").slideUp("fast");
+            $("#modalContainer").slideUp("fast");
+        }
     });
 
 
-    $(".edit").click(function() {
+    $(document).on('click', '.edit', function() {
         var target = event.target.id;
         var foredit =  $("#" + target + " > label").text()
         console.log(target);
@@ -53,18 +69,61 @@ $(document).ready(function(){
         $("#modal").slideDown("fast");
         $("#modalContainer").slideDown("fast");
 
-        $("#editTodo").click(function(){
+        $(document).one('click', '#editTodo', function(){
             var editedTodo =  $("#text").val();
+            console.log(target);
             $("#" + target + " > label").text(editedTodo)
             $("#modal").slideUp("fast");
             $("#modalContainer").slideUp("fast");
+            $("#text").val('');
         });
       });
 
-     
 
+      $(document).on('click', '.edit', function() {
+        var target = event.target.id;
+        var foredit =  $("#" + target + " > label").text()
+        console.log(target);
+        console.log(foredit);
+      });
 
-    
-    
+      // delete
+      $(document).on('click', '.delete', function() {
+        var rem = event.target.id;
+        console.log(rem);
+        $("li#"+rem).remove()
+      });
 
+      //set to in progress
+      $(document).on('click', '.stat_progress', function() {
+        var id = event.target.id;
+        console.log(id);
+        $("li#"+id).prependTo('ul.progress')
+        $("li#"+id+" a.stat_todo").show();
+        $("li#"+id+" a.stat_progress").hide(); 
+        $("li#"+id+" a.stat_done").show();
+        $("li#"+id+" a.delete").hide();
+      });
+
+      //set to in todo
+      $(document).on('click', '.stat_todo', function() {
+        var id = event.target.id;
+        console.log(id);
+        $("li#"+id).prependTo('ul.todo')
+        $("li#"+id+" a.stat_todo").hide();
+        $("li#"+id+" a.stat_progress").show(); 
+        $("li#"+id+" a.stat_done").hide();
+        $("li#"+id+" a.delete").show();
+      });
+
+       //set to done
+       $(document).on('click', '.stat_done', function() {
+        var id = event.target.id;
+        console.log(id);
+        $("li#"+id).prependTo('ul.done')
+        $("li#"+id+" a.stat_todo").show();
+        $("li#"+id+" a.stat_progress").show(); 
+        $("li#"+id+" a.stat_done").hide();
+        $("li#"+id+" a.delete").show();
+      });
 });
