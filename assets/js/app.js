f479df0a74dbdd99b122b2d02437b087cb2df459
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-06-04 13:16:54 +0800

    Finished the todo challenge

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,12 @@ $(document).ready(function() {
     
 });
 
+//Removes the task
 function removeT(x) {
     $(`.task${x}`).remove();
 }
 
+//Moves the task forward the line
 function moveFor(x) {
     let str = $(`.task${x}`).data("pos");
     let task = $(`.task${x}`).text();
@@ -39,18 +41,23 @@ function moveFor(x) {
     }
 }
 
-function moveBack() {
+//Moves the task backward
+function moveBack(x) {
     let str = $(`.task${x}`).data("pos");
     let task = $(`.task${x}`).text();
     let htm = $(`.task${x}`).detach();
     if(str === "prog") {
         $('.unord-todo').prepend(htm);
+        $(`.task${x}`).data("pos","todo");
+    } else if(str === "done") {
+        $('.unord-prog').prepend(htm);
+        $(`.task${x}`).data("pos","prog");
     }
 }
 
 function addTask(task) {
     $('.unord-todo').prepend(`
-    <li data-id="${taskID}" data-pos="todo" class="task${taskID}">${task}<i class="fa fa-lg fa-trash-o"  onclick="removeT(${taskID});" title="Delete the task"></i><i title="Move it one step back" class="fa fa-lg fa-eraser"></i><i title="Move task forward" onclick="moveFor(${taskID});" class="fa fa-lg fa-check-square-o"></i></li>
+    <li data-id="${taskID}" data-pos="todo" class="task${taskID}"><p contenteditable=true title="Click to edit text.">${task}</p><span><i class="fa fa-lg fa-trash-o"  onclick="removeT(${taskID});" title="Delete the task"></i><i title="Move it one step back" onclick="moveBack(${taskID})" class="fa fa-lg fa-eraser"></i><i title="Move task forward" onclick="moveFor(${taskID});" class="fa fa-lg fa-check-square-o"></i></span></li>
     `);
     taskID+=1;
 }
