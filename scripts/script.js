934ef373d95c26332437fc22b0c73b45808972a5
Bryan Alfuente <bryan.alfuente@boom.camp>
2019-06-04 00:55:06 -0700

    submission

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,37 @@
-const list = [];
-
-function Task(task) {
-    this.task = task;
-    this.id = 'new-todo';
-  }
-
-  var newTask = document.querySelector('.taskSection')
-  
-  function addTask(task) {
-    if (task) {
-      const newTask = new Task(task);
-      list.push(newTask);
-      document.getElementById('newInput').value = '';
-  
-      const taskElem = document.createElement('li');
-      taskElem.id = 'item';
-  
-      const item = document.getElementById('newList').appendChild(taskElem);
-      item.innerHTML = 
-      `<a id="new-todo" class="list-item">
-        ${newTask.task}
-        </a>` + `<br><button id="progress" class="list-button progress">
-        In-progress
-        </button>` + `<button id="done" class="list-button done">
-        Done
-        </button>`
-        ;
-    }
-  }
-
-  document.getElementById('submit')
-  .addEventListener('click', function(event) {
-    event.preventDefault();
-    const task = document.getElementById('newInput').value.trim();
-    addTask(task);
-  });
-
-
-  var buttonProgress = document.getElementById('newList');
-  buttonProgress.addEventListener('click', function(event) {
-    if (event.target.matches('list-item')) {
-        event.target.id = 'inProgress';
-        document.getElementById('currentList').append(event.target);
-      }
-  });
-
-  var matchProgress = document.getElementById('currentList');
-  matchProgress.addEventListener('click', function(event) {
-    if (event.target.matches('list-item')) {
-      event.target.id = 'archived';
-      document.getElementById('archivedList').append(event.target);
-    }
-  });
-
 $(document).ready(function(){
-    $('.progress').on('click', () => {
-        
+    var tasks = [];
+    var i = 1;
+    $('#add-btn').on('click', function() {
+        var newTask = $('.input-task').val();
+        tasks.push(newTask);
+        var addTask = `
+        <div class="box-item">
+            <label class="task-name" for="styled-checkbox-1">${newTask}${i}</label>
+            <img class="trash" src="img/trash.png" alt="">
+            <img class="prog-img" id="task${i}" src="img/progress.png" alt="">
+        </div>
+        `
+        $('.new-task').append(addTask);
+        $('.input-task').val('');
+      x++;
+    })
+    
+      $('.box-list').on('click', '.trash',function(){
+        $(this).parent().remove();
+    });
+
+    $('.new-task').on('click', '.prog-img',function(){
+        $('.in-progress').append($(this).parent());
     })
 
+
+    $('.in-progress').on('click', '.prog-img',function(){
+        $('.done').append($(this).parent());
+        $('.task-name').css('text-decoration', 'line-through')
+    })
+
+    $('.done').on('click', '.fa-angle-double-left',function(){
+      $('.progress').append($(this).parent());
+      $('.task-name').css('text-decoration', 'none')
+    })
 })
