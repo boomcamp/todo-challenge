283511458fe5b580d2ae175c94d093fce269b10e
romeoaaron21 <romeo.lumibao@boom.camp>
2019-06-04 12:48:06 +0800

    Todo-challenge

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ $(document).ready(() => {
     var x = 0;
     $('.add').on('click', () => {
         var $done = $('input').val();
-        
+
+            
            var $nDone = `<div class="con1-task" id="${x}">`+
-           '<span>'+$done+'</span>'+'<br/>'+
-           `<span><button id="${x}" class="remove" ><i class="fa fa-trash"></i>Remove</button></span>`+
-           `<span><button id="${x}" class="progress" ><i class="fa fa-tasks"></i> Progress</button></span>`+
-           `<span><button id="${x}" class="done" ><i class="fa fa-check"></i>Done</button></span>`+  
+           
+           '<span class="span-edit">'+$done+'</span>'+'<br/>'+
+           `<span><button id="${x}" class="revert" ><i class="fa fa-backward"></i></button></span>`+ 
+           `<span><button id="${x}" class="remove" ><i class="fa fa-trash"></i></button></span>`+
+           `<span><button id="${x}" class="progress" ><i class="fa fa-tasks"></i></button></span>`+
+           `<span><button id="${x}" class="done" ><i class="fa fa-check"></i></button></span>`+
+           `<span><button id="${x}" class="edit" ><i class="fa fa-edit"></i></button></span>`+  
            '</div>';
     
            $('.list').append($nDone);
@@ -39,24 +43,59 @@ $(document).ready(() => {
 
     $('.list').on('click', '.progress', function() {
         $('.list1').append($(this).parent().parent());
-        $('.progress').hide();
+
     });
 
     $('.list1').on('click', '.done', function() {
         $('.list2').append($(this).parent().parent());
-        $('.progress').hide();
-        $('.done').hide();
+
     });
 
     $('.list').on('click', '.done', function() {
         $('.list2').append($(this).parent().parent());
-        $('.progress').hide();
-        $('.done').hide();
+    
     });
 
     //end move
 
 
+    //start edit
+
+    $('.list').on('click', '.edit', function() {
+        var id = $(this).attr('id');
+        $('.span-edit').attr('contenteditable', 'true');
+        $('.span-edit').focusOut();
+    })
+
+    $('.list1').on('click', '.edit', function() {
+        var id = $(this).attr('id');
+        $('.span-edit').attr('contenteditable', 'true');
+        $('.span-edit').focusOut();
+    })
+
+    $('.list2').on('click', '.edit', function() {
+        var id = $(this).attr('id');
+        $('.span-edit').attr('contenteditable', 'true');
+        $('.span-edit').focus();
+    })
+
+    //end edit
+
+
+    //start back
+
+    $('.list1').on('click', '.revert', function() {
+        $('.list').append($(this).parent().parent());
+
+    });
+
+    $('.list2').on('click', '.revert', function() {
+        $('.list1').append($(this).parent().parent());
+    
+    });
+
+
+    //end back
     
 
     
