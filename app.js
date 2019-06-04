aabff5bf5d1f6a3e6e2f6e8c041526d0bfd99489
jomar-09 <jomar2999@gmail.com>
2019-06-04 15:46:03 +0800

    added local storage

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const list = JSON.parse(localStorage.getItem('task')) || [];
 $(document).ready(function(){
     $('#todo').click(function(){
         $('.done').removeClass('selected');
@@ -26,29 +27,52 @@ $(document).ready(function(){
     $(document).on("click", "button.delete", function(){
         //  alert("clicked");
         $(this).parent().remove();
+        list.forEach((val, i) => {
+            if(val.task==$(this).siblings('span').text()){
+                list.splice(i, 1);
+            }
+        });
+        localStorage.setItem('task', JSON.stringify(list));
     });
     $(document).on("click", "button.move1", function(){
         //   alert("clicked");
+        list.map(val => {
+            if(val.task==$(this).siblings('span').text()){
+                val.stat='in-progress';
+            }
+        });
+        localStorage.setItem('task', JSON.stringify(list));
         var newHtml = `<li><span contenteditable="true">${$(this).siblings('span').text()}</span><button title=" to todo" class="move3">&lsaquo;</button><button title="to done" class="move2">&rsaquo;</button></li>`;
         $('.in-progress .list').append(newHtml);
         $(this).parent().remove();
     });
     $(document).on("click", "button.move2", function(){
         //   alert("clicked");
+        list.map(val => {
+            if(val.task==$(this).siblings('span').text()){
+                val.stat='done';
+            }
+        });
+        localStorage.setItem('task', JSON.stringify(list));
          var newHtml = `<li><span contenteditable="true">${$(this).siblings('span').text()}</span><button class="delete" title="remove">&times;</button><button title=" to in progress" class="move1">&lsaquo;</button></li>`;
         $('.done .list').append(newHtml);
         $(this).parent().remove();
     });
     $(document).on("click", "button.move3", function(){
         //   alert("clicked");
+        list.map(val => {
+            if(val.task==$(this).siblings('span').text()){
+                val.stat='in-progress';
+            }
+        });
+        localStorage.setItem('task', JSON.stringify(list));
          var newHtml = `<li><span contenteditable="true">${$(this).siblings('span').text()}</span><button class="delete" title="remove">&times;</button><button title=" to in progress" class="move1">&rsaquo;</button></li>`;
         $('.todo .list').append(newHtml);
         $(this).parent().remove();
     });
-    const list = [];
     function Task(task) {
         this.task = task;
-        this.id = 'new';
+        this.stat = 'new';
     }
     
     function addTask(task) {
@@ -66,7 +90,28 @@ $(document).ready(function(){
     $('#add').click(function(){
         const task = $('input').val().trim();
         addTask(task);
+        localStorage.setItem('task', JSON.stringify(list));
+
     });
     
+    
+    list.forEach(val => {
+        if(val.stat =='new'){
+            var newHtml = `<li><span contenteditable="true">${val.task}</span><button class="delete" title="remove">&times;</button><button title=" to in progress" class="move1">&rsaquo;</button></li>`;
+            $('.todo .list').append(newHtml);
+        }
+        else if(val.stat =='in-progress'){
+            var newHtml = `<li><span contenteditable="true">${val.task}</span><button title=" to todo" class="move3">&lsaquo;</button><button title="to done" class="move2">&rsaquo;</button></li>`;
+            $('.in-progress .list').append(newHtml);
+        }
+        else if(val.stat =='done'){
+            var newHtml = `<li><span contenteditable="true">${val.task}</span><button class="delete" title="remove">&times;</button><button title=" to in progress" class="move1">&lsaquo;</button></li>`;
+            $('.done .list').append(newHtml);
+        }
+        
+    });
+
+
+
     
 })
