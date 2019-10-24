657236dc1bda9aef98e83b1f4dda732025e39ffc
clarkamor <clark.amor@boom.camp>
2019-10-24 18:02:01 +0800

    submission

--- app.js
+++ app.js
@@ -14,35 +14,3 @@ function checkTime(i) {
   } // add zero in front of numbers < 10
   return i;
 }
-
-const list = [];
-function Task(task) {
-  this.task = task;
-  this.id = "new";
-}
-function addTask(task) {
-  if (task) {
-    const newTask = new Task(task);
-    list.push(newTask);
-    document.getElementById("my-input").value = "";
-
-    const taskElem = document.createElement("li");
-    taskElem.id = "item";
-
-    const item = document.getElementById("newList").appendChild(taskElem);
-    item.innerHTML = `
-          <a id="new" class="list-group-item">
-            ${newTask.task}
-          </a>
-        `;
-  }
-  newTaskForm.classList.toggle("hide");
-}
-
-document
-  .getElementById("add-button")
-  .addEventListener("click", function(event) {
-    event.preventDefault();
-    const task = document.getElementById("my-input").value.trim();
-    addTask(task);
-  });
