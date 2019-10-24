a91fc1e91c6c84c7ccf353570ef81593020a327f
clarkamor <clark.amor@boom.camp>
2019-10-24 17:45:14 +0800

    submission

--- app.js
+++ app.js
@@ -14,11 +14,35 @@ function checkTime(i) {
   } // add zero in front of numbers < 10
   return i;
 }
-function myFunction() {
-  var x = document.getElementById("toggle");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+
+const list = [];
+function Task(task) {
+  this.task = task;
+  this.id = "new";
 }
+function addTask(task) {
+  if (task) {
+    const newTask = new Task(task);
+    list.push(newTask);
+    document.getElementById("my-input").value = "";
+
+    const taskElem = document.createElement("li");
+    taskElem.id = "item";
+
+    const item = document.getElementById("newList").appendChild(taskElem);
+    item.innerHTML = `
+          <a id="new" class="list-group-item">
+            ${newTask.task}
+          </a>
+        `;
+  }
+  newTaskForm.classList.toggle("hide");
+}
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", function(event) {
+    event.preventDefault();
+    const task = document.getElementById("my-input").value.trim();
+    addTask(task);
+  });
