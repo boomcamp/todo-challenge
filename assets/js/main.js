7d192a2c0c274010b407a3aa5a769e7c48353cb5
marcial norte <marcial.norte@boom.camp>
2019-10-25 15:39:25 +0800

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 var c = 0;
+var countTodo = 0;
+var countInp = 0;
+var countDone = 0;
 document.getElementById("add").style.display = "none";
 
 var a = document.getElementById("newtask");
@@ -22,19 +25,179 @@ document.getElementById("add").addEventListener("click", function() {
 
 	parent.insertAdjacentHTML(
 		"afterend",
-		`<tr><td id="content${c}" class="list-group-item">${task}</td><td>&#9997;</td><td id="new">&#10004;</td><td>&#10060;</td></tr>`
+		`<tr><td id="content${c}" class="list-group-item" width="90%">${task}</td><td>&#9997;</td><td class="delete">&#10060;</td></tr>`
 	);
 	c++;
+	countTodo++;
+
+	if (countTodo > 0) {
+		document.getElementById("empty").style.display = "none";
+		if (document.getElementById("empty2")) {
+			document.getElementById("empty2").style.display = "none";
+		}
+	}
+	showcount();
 });
 
-// document.getElementById("todo").addEventListener("click", function(event) {
-// 	console.log(event.target);
-// 	if (event.target.matches("td.list-group-item")) {
-// 		console.log("jhi");
-// 		var parent = document.getElementById("inprogress");
+document.getElementById("todo").addEventListener("click", function(event) {
+	console.log(event.target);
+	if (event.target.matches("td.list-group-item")) {
+		document
+			.getElementById("inprogress")
+			.insertAdjacentHTML(
+				"afterend",
+				`<tr><td id="content${c}" class="list-group-item" width="90%">${event.target.innerText}</td><td id="back">&#171;</td><td>&#9997;</td><td class="delete">&#10060;</td></tr>`
+			);
+		event.target.parentElement.remove();
+		countTodo--;
+		showMessage();
 
-// 		var inprog = document.getElementById("content").innerText;
+		countInp++;
+		if (countInp > 0) {
+			document.getElementById("emptyInp").style.display = "none";
+		}
+	}
+	if (event.target.matches("td.delete")) {
+		if (confirm("Are you sure to delete?")) {
+			event.target.parentElement.remove();
+			countTodo--;
+			showMessage();
+		} else false;
+	}
+	showcount();
+});
 
-// 		document.getElementById("inp").append(event.target);
-// 	}
-// });
+document.getElementById("inp").addEventListener("click", function(event) {
+	if (event.target.matches("td.list-group-item")) {
+		document
+			.getElementById("donetask")
+			.insertAdjacentHTML(
+				"afterend",
+				`<tr><td id="content${c}" class="list-group-item" width="90%">${event.target.innerText}</td><td id="up">&#8648;</td><td>&#9997;</td><td class="delete">&#10060;</td></tr>`
+			);
+		event.target.parentElement.remove();
+		countInp--;
+		countDone++;
+
+		if (countDone > 0) {
+			document.getElementById("emptyDone").style.display = "none";
+		}
+
+		if (countInp == 0) {
+			document.getElementById("emptyInp").style.display = "table-row";
+		}
+	}
+	if (event.target.matches("td#back")) {
+		//console.log(event.target.parentElement.querySelector("td").innerText);
+		document
+			.getElementById("dolist")
+			.insertAdjacentHTML(
+				"afterend",
+				`<tr><td id="content${c}" class="list-group-item" width="90%">${
+					event.target.parentElement.querySelector("td").innerText
+				}</td><td>&#9997;</td><td class="delete">&#10060;</td></tr>`
+			);
+
+		countInp--;
+		countTodo++;
+		event.target.parentElement.remove();
+
+		if (countTodo > 0) {
+			document.getElementById("empty").style.display = "none";
+		}
+
+		if (countInp == 0) {
+			document.getElementById("emptyInp").style.display = "table-row";
+		}
+	}
+	if (event.target.matches("td.delete")) {
+		if (confirm("Are you sure to delete?")) event.target.parentElement.remove();
+		else false;
+		countInp--;
+		if (countInp > 0) {
+			document.getElementById("emptyInp").style.display = "none";
+		} else {
+			document.getElementById("emptyInp").style.display = "table-row";
+		}
+	}
+	showcount();
+});
+
+document.getElementById("done").addEventListener("click", function(event) {
+	if (event.target.matches("td#up")) {
+		console.log(true);
+
+		document
+			.getElementById("inprogress")
+			.insertAdjacentHTML(
+				"afterend",
+				`<tr><td id="content${c}" class="list-group-item" width="90%">${
+					event.target.parentElement.querySelector("td").innerText
+				}</td><td id="back">&#171;</td><td>&#9997;</td><td class="delete">&#10060;</td></tr>`
+			);
+
+		countDone--;
+		countInp++;
+		event.target.parentElement.remove();
+		if (countInp > 0) {
+			document.getElementById("emptyInp").style.display = "none";
+		}
+		if (countDone == 0) {
+			document.getElementById("emptyDone").style.display = "table-row";
+		}
+	}
+	if (event.target.matches("td.delete")) {
+		if (confirm("Are you sure to delete?")) {
+			event.target.parentElement.remove();
+			countDone--;
+			if (countDone > 0) {
+				document.getElementById("emptyDone").style.display = "none";
+			} else {
+				document.getElementById("emptyDone").style.display = "table-row";
+			}
+		} else false;
+	}
+	showcount();
+});
+function showMessage() {
+	if (countTodo > 0) {
+		document.getElementById("empty").style.display = "none";
+	} else {
+		document.getElementById("empty").style.display = "table-row";
+	}
+}
+
+function showTime() {
+	var date = new Date();
+	var h = date.getHours(); // 0 - 23
+	var m = date.getMinutes(); // 0 - 59
+	var s = date.getSeconds(); // 0 - 59
+	var session = "AM";
+
+	if (h == 0) {
+		h = 12;
+	}
+
+	if (h > 12) {
+		h = h - 12;
+		session = "PM";
+	}
+
+	h = h < 10 ? "0" + h : h;
+	m = m < 10 ? "0" + m : m;
+	s = s < 10 ? "0" + s : s;
+
+	var time = h + ":" + m + ":" + s + " " + session;
+	document.getElementById("MyClockDisplay").innerText = time;
+	document.getElementById("MyClockDisplay").textContent = time;
+
+	setTimeout(showTime, 1000);
+}
+
+showTime();
+showcount();
+function showcount() {
+	document.getElementById("cDolist").innerHTML = countTodo;
+	document.getElementById("cInp").innerHTML = countInp;
+	document.getElementById("cDone").innerHTML = countDone;
+}
